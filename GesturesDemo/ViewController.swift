574991118154d0e0740ae5aea614e2d37fcd072d
vincvincedell <vince.dell3@gmail.com>
2020-10-15 09:44:43 -0400

    Initial Commit

---
+++ GesturesDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GesturesDemo
+//
+//  Created by Vincent Dell'Arciprete on 10/15/20.
+//  Copyright © 2020 Vincent Dell'Arciprete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
