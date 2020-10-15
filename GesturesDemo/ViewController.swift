13ec403d8b4fac074e1b2ae956edd6b56665e982
vincvincedell <vince.dell3@gmail.com>
2020-10-15 10:30:23 -0400

    MVP

--- GesturesDemo/ViewController.swift
+++ GesturesDemo/ViewController.swift
@@ -8,13 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func imageviewTapped(_ sender: UITapGestureRecognizer)
+    {
+        view.backgroundColor = UIColor.cyan
+    }
+    
 
+    @IBAction func emojiTapped(_ sender: UIPanGestureRecognizer)
+    {
+        // 1
+        let translation = sender.translation(in: view)
+
+        // 2
+        guard let gestureView = sender.view else {
+          return
+        }
+
+        gestureView.center = CGPoint(
+          x: gestureView.center.x + translation.x,
+          y: gestureView.center.y + translation.y
+        )
+
+        // 3
+        sender.setTranslation(.zero, in: view)
 }
 
+}
